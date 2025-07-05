cf21968715a55ccf67aa7bb7ba1c781ce9bbd159
Aryan <abardhan@student.42abudhabi.ae>
2025-07-05 16:35:39 +0400

    Libft

--- minitalk.h
+++ minitalk.h
@@ -6,12 +6,17 @@
 /*   By: aryan <aryan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 16:45:45 by aryan             #+#    #+#             */
-/*   Updated: 2025/07/03 16:55:17 by aryan            ###   ########.fr       */
+/*   Updated: 2025/07/05 16:24:28 by aryan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+
+#ifndef MINITALK_H
+#define MINITALK_H
 
 #include <signal.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include "libft/libft.h"
 
+#endif
